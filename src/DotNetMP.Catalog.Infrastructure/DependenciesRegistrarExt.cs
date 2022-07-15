7a4b1c3160f215db666f38a49b7b9fd12781d187
Siarhei Sialitski <siarhei.sialitski.ws@gmail.com>
2022-07-15 10:32:53 +0300

    Message based communication implemented

--- src/DotNetMP.Catalog.Infrastructure/DependenciesRegistrarExt.cs
+++ src/DotNetMP.Catalog.Infrastructure/DependenciesRegistrarExt.cs
@@ -1,6 +1,9 @@
-﻿using DotNetMP.Catalog.Infrastructure.Data;
+﻿using Azure.Messaging.ServiceBus;
+using DotNetMP.Catalog.Infrastructure.Data;
+using DotNetMP.Catalog.Infrastructure.ServiceBus;
 using DotNetMP.SharedKernel.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -15,6 +18,21 @@ public static class DependenciesRegistrarExt
         });
 
         services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+
+        services.AddAzureClients(builder =>
+        {
+            builder.AddServiceBusClient(configuration.GetConnectionString("ServiceBusConnectionString"))
+                .ConfigureOptions(configuration =>
+                {
+                    configuration.RetryOptions = new ServiceBusRetryOptions()
+                    {
+                        Mode = ServiceBusRetryMode.Exponential
+                    };
+                });
+        });
+
+        services.AddTransient<IServiceBusRepository, ServiceBusRepository>();
+
         return services;
     }
 }
--- src/DotNetMP.Catalog.WebApi/Application/Commands/UpdateItemCommandHandler.cs
+++ src/DotNetMP.Catalog.WebApi/Application/Commands/UpdateItemCommandHandler.cs
@@ -1,8 +1,11 @@
 ﻿using DotNetMP.Catalog.Core.Aggregates.CategoryAggregate;
 using DotNetMP.Catalog.Core.Aggregates.ItemAggregate;
+using DotNetMP.Catalog.Infrastructure.ServiceBus;
 using DotNetMP.SharedKernel.Exceptions;
+using DotNetMP.SharedKernel.IntegrationEvents;
 using DotNetMP.SharedKernel.Interfaces;
 using MediatR;
+using Newtonsoft.Json;
 
 namespace DotNetMP.Catalog.WebApi.Application.Commands;
 
@@ -10,36 +13,42 @@ public class UpdateItemCommandHandler : IRequestHandler<UpdateItemCommand>
 {
     private readonly IRepository<Item> _itemsRepository;
     private readonly IRepository<Category> _categoriesRepository;
+    private readonly IServiceBusRepository _serviceBusRepository;
 
     public UpdateItemCommandHandler(
         IRepository<Item> itemsRepository,
-        IRepository<Category> categoriesRepository)
+        IRepository<Category> categoriesRepository,
+        IServiceBusRepository serviceBusRepository)
     {
         _itemsRepository = itemsRepository;
         _categoriesRepository = categoriesRepository;
+        _serviceBusRepository = serviceBusRepository;
     }
 
-    public async Task<Unit> Handle(UpdateItemCommand request, CancellationToken cancellationToken)
+    public async Task<Unit> Handle(UpdateItemCommand itemToUpdate, CancellationToken cancellationToken)
     {
-        var item = await _itemsRepository.GetByIdAsync(request.Id, cancellationToken);
+        var item = await _itemsRepository.GetByIdAsync(itemToUpdate.Id, cancellationToken);
         if (item == null)
         {
             throw new NotFoundException();
         }
 
-        var category = await _categoriesRepository.GetByIdAsync(request.CategoryId, cancellationToken);
+        var category = await _categoriesRepository.GetByIdAsync(itemToUpdate.CategoryId, cancellationToken);
         if (category == null)
         {
             throw new NotFoundException();
         }
 
-        item.UpdateName(request.Name);
-        item.UpdateDescription(request.Description);
-        item.UpdatePrice(request.Price);
-        item.UpdateImage(request.Image);
+        item.UpdateName(itemToUpdate.Name);
+        item.UpdateDescription(itemToUpdate.Description);
+        item.UpdatePrice(itemToUpdate.Price);
+        item.UpdateImage(itemToUpdate.Image);
         item.UpdateCategory(category);
 
         await _itemsRepository.UpdateAsync(item, cancellationToken);
+
+        var itemUpdated = new ItemUpdatedIntegrationEvent(item.Id, item.Name, item.Price, item.Description, item.Image);
+        await _serviceBusRepository.SendMessageAsync(JsonConvert.SerializeObject(itemUpdated), cancellationToken);
 
         return Unit.Value;
     }
